ace69df67380a30e8167085fcd48b59893b4084a
asghar <asghar>
2015-03-12 17:59:30 +0330

    complit tf version

--- keyword-extraction/main.py
+++ keyword-extraction/main.py
@@ -28,24 +28,27 @@ def getWordsOftext(text):#get a text by file.read()
 	return all words of string as a dictionary : word -> [count, weight]
 	"""
 	textWords = {}#word -> [count, weight]
-	splitedt = text.split("\s") 
+	splited = text.split(" ") 
 	bigooDchars = ['', '\n', '\s']
 	for i in range(len(splited) - 1, -1, -1):
 		if splited[i] in bigooDchars:
 			del splited[i]
-	print ("splited is ", splited)
-	for word in slpited:
-		textWords[word] = textWords.get(word, [0, 0])[0] + 1
+	#print ("splited is ", splited)
+	for word in splited:
+		#print ("##### word is ", word)
+		textWords[word] = [textWords.get(word, [0, 0])[0] + 1, 0]
 	return textWords
 
 def getMaxOfWords(words):
 	"""
 	return maximum of a dictionary by word_count as a toples
 	"""
+	print ("###words is : ", words) 
 	maxWord = ('', 0)
 	for word in words:
-		if word[0] > maxWord[1]:
-			maxWord = (word, word[0])
+		print ("$$word is : ", word)
+		if words[word][0] > maxWord[1]:
+			maxWord = (word, words[word][0])
 	return maxWord
 	
 def getNMAxOFWordsByWeight(words, n):
@@ -57,9 +60,11 @@ def getNMAxOFWordsByWeight(words, n):
 		maxx = ('', 0)#string , weight
 		for word in words:
 			if words[word][1] > maxx[1]:
-				maxx = (word, word[1])
-		maxes[maxx[0]] = words[word]
-		del words[word]
+				maxx = (word, words[word][1])
+		maxes[maxx[0]] = words[maxx[0]]
+		print("$$$adding   :  ", maxx[0]) 
+		del words[maxx[0]]
+	print ("&&&in last function", maxes)
 	return maxes
 	
 def getKeyWords(doc, keyWordCount):
@@ -70,5 +75,11 @@ def getKeyWords(doc, keyWordCount):
 	maxOfDocWords = getMaxOfWords(docWords)
 	k = .5
 	for word in docWords:
-		docWords[word][1] = k + (1 - k) * docWords[word][0] / MaxOfDocWords
-	getNMAxOFWordsByWeight(docWords, keyWordCount)
+		docWords[word][1] = k + (1 - k) * docWords[word][0] / maxOfDocWords[1]
+	return getNMAxOFWordsByWeight(docWords, keyWordCount)
+
+###### 
+
+allwords = getAllWordsFromFile()
+testDoc_file = open("test.txt", 'r')
+print (getKeyWords(testDoc_file.read(), 3))
