f58a069c6586f31dc02ebf597a9b54f2ab6cd20e
mrBigooD <milad.mr95@gmail.com>
2015-03-19 01:04:56 +0330

    add getUserAttraction and improve main loop

--- keyword-extraction/main.py
+++ keyword-extraction/main.py
@@ -6,18 +6,51 @@ global programmingWords#
 footballsWords =  pickle.load(open("footballWords.st", "rb"))
 programmingWords = pickle.load(open("programmingWords.st", "rb"))
 
-def getTwittAttract(keywords):
-	attraction = [0, ,0]
+def getTwittAttraction(twitt):
+	global footballsWords#textlist
+	global programmingWords#
+	attraction = [0, 0]#[football, programming]
+	football = 0
+	programming = 1
+	keywords = extraction.getKeyWords(twitt, 4)
 	for word in keywords:
 		if keywords[word] in footballsWords:
-			
-	
+			attraction[football] +=1
+		if keywords[word] in programmingWords:
+			attraction[programming] +=1
+	if attraction[football] == attraction[programming]:
+		if  attraction[football] == 0:
+			return [0, 0]
+		return [1, 1]	
+	if attraction[football] > attraction[programming]:
+		for word in keywords:
+			if word not in footballsWords:
+				footballsWords.append(word)
+		return [1, 0]
+	else :
+		for word in keywords:
+			if word not in programmingWords:
+				programmingWords.append(word)
+		return [0, 1]
+		
+def getUserAttraction(user):
+	user_attraction = [0, 0]
+	twitts = user.get_twitt()
+	for twitt in twitss:
+		temp_attraction = getTwittAttraction(twitt)
+		user_attraction[0] += temp_attraction[0]
+		user_attraction[1] += temp_attraction[1]
+	return [user_attraction[0] / len(twitts), user_attraction[1] / len(twitts), len(twitts)]
+		
+		
 passedUsers = []
 inQueueUsers = [] 
-while (len(users_queue)>0):
+while (len(inQueueUsers) > 0):### in bayad bere tu thread (ye lock ham bayad bara in lista accuire beshe)
 	user = inQueueUsers[0]
-	twitts = user.get_twitt()
-	keywords = []
-	for twitt in twitss:
-		keyWords.append(extraction.getKeyWords(twitt, 3)
-		
+	user.attraction = getUserAttraction(user)
+	inQueueUsers += user.get_flwing
+	passedUsers.append(user)
+	del inQueueUsers[user]
+
+pickle.dump(footballsWords, open("footballWords.st", "wb"))
+pickle.dump(programmingWords, open("programmingWords.st", "wb"))
--- keyword-extraction/scraper.py
+++ keyword-extraction/scraper.py
@@ -2,15 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import urllib2
-import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
 
+userName = 'armanjtehrani'#raw_input('please enter your user name & password: \n')
+passWord = 'POISALA'#raw_input()
 browser = webdriver.Firefox()
 browser.get("http://www.twitter.com")
 username = browser.find_element_by_id("signin-email")
 password = browser.find_element_by_id("signin-password")
 
-username.send_keys("ArmanJTehrani@Outlook.com")
-password.send_keys("POISALA",Keys.ENTER)
+username.send_keys(userName)
+password.send_keys(passWord,Keys.ENTER)
 cookies = browser.get_cookies()
 for i in cookies :
     if i['name'] == 'auth_token' :
@@ -21,12 +23,12 @@ def setup_opener():
     opener = urllib2.build_opener(proxy)
     urllib2.install_opener(opener)
 
-    opener.addheaders.append(('Cookie', 'auth_token=c8f9728c372e61ba5703bec8551601e529bb18c4'))
+    opener.addheaders.append(('Cookie', 'auth_token=' + value))
     return opener
-
+opener = setup_opener()
 class Profile:
-    def get_twitt(self,name = None):
-        profile = opener.open("https://twitter.com/" + name)
+    def get_twitt(self):
+        profile = opener.open('https://twitter.com/'+self.name)
         prohttp = profile.read()
         soup = BeautifulSoup(prohttp)
         twitt = soup.findAll('p',{'class':'ProfileTweet-text js-tweet-text u-dir','lang':'en','data-aria-label-part':'0','dir':'ltr'})
@@ -34,33 +36,46 @@ class Profile:
             twitt[i] = twitt[i].text
         return twitt
 
-    def get_flwing(self,name = None):
-        profile = opener.open("https://twitter.com/" + name + "/following")
+    def get_flwing(self):
+        profile = opener.open('https://twitter.com/' + self.name + '/following')
         prohttp = profile.read()
         soup = BeautifulSoup(prohttp)
         flwing = soup.findAll('span',{'class':'u-linkComplex-target'})
         for i in range(len(flwing)):
             flwing[i] = flwing[i].text
         return flwing
-    def get_flwer(self,name = None):
-        profile = opener.open("https://twitter.com/" + name + "/followers")
+    def get_flwer(self):
+        profile = opener.open('https://twitter.com/' + self.name + '/followers')
         prohttp = profile.read()
         soup = BeautifulSoup(prohttp)
-    def __init__(self,name = None,rel = None):
+        flwer = soup.findAll('span',{'class':'u-linkComplex-target'})
+        for i in range(len(flwer)):
+            flwer[i] = flwer[i].text
+        return flwer
+    def __init__(self,name=""):
         self.name = name
-        self.rel = rel
-        self.twitt = get_twitt(name)
-        self.flwing = get_flwing(name)
-        self.attract = False
+    #    self.rel = rel
+     #   self.twitt = self.get_twitt(name)
+      #  self.flwing = self.get_flwing(name)
+       # self.flwer = self.get_flwer(name)
+        self.attract = None #[football, programming, totalTwittCount]
 
 
 myPro = Profile()
 pro = []
+
 for i in range(len(myPro.flwing)):
-    pro.append(Profile(mypro.flwing[i]))
-    check = 0
-while(True):
-    for i in pro:
-        milad(i)
-        for i in range(len(Pro[check].flwing)):
-            pro.append(Profile(Pro[check].flwing[i]))
+    pro.append(Profile(myPro.flwing[i],userName))
+    pause = False
+for user in pro:
+    while not pause:
+        #milad(user)
+        for i in range(len(user.flwing)):
+            for j in pro:
+                if j.name == user.flwing[i]:
+                    j.rel.append(user.name)
+                    break
+            else: 
+                pro.append(Profile(user.flwing[i],))
+
+        
